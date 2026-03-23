beecf1cdc4f848819555c13eac48c0033ba5d1d2
kris1027 <krzys89@gmail.com>
2026-03-23 09:37:25 +0100

    feat: add robots.txt with disallowed private routes and NEXT_PUBLIC_SITE_URL env var

---
+++ src/app/robots.ts
@@ -0,0 +1,23 @@
+import type { MetadataRoute } from 'next';
+
+import { env } from '@/config/env';
+
+const robots = (): MetadataRoute.Robots => ({
+  rules: {
+    userAgent: '*',
+    allow: '/',
+    disallow: [
+      '/account/',
+      '/cart/',
+      '/checkout/',
+      '/login/',
+      '/register/',
+      '/forgot-password/',
+      '/reset-password/',
+      '/verify-email/',
+    ],
+  },
+  sitemap: `${env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
+});
+
+export default robots;
